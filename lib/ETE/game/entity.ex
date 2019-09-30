99e9c3c2d8a6d6984565251e9b6db875c61594a1
Craig Dazey <craigdazey@gmail.com>
2019-09-29 23:38:14 -0400

    add not-so-great movement queueing

--- lib/ETE/game/entity.ex
+++ lib/ETE/game/entity.ex
@@ -11,23 +11,107 @@ defmodule ETE.Game.Entity do
             width: @width,
             vx: 0,
             vy: 0,
-            speed: 5
+            speed: 5,
+            latest_x_movement: nil,
+            latest_y_movement: nil,
+            x_movement: [],
+            y_movement: []
 
   def set_moving(%__MODULE__{speed: speed} = player, orientation) do
+    # We need to use Enum.uniq here because we sometimes get multiple key_down's in
+    # the same direction before we get a key_up
     case orientation do
-      :up -> %{player | vy: -speed}
-      :down -> %{player | vy: speed}
-      :left -> %{player | vx: -speed}
-      :right -> %{player | vx: speed}
+      :up ->
+        %{
+          player
+          | vy: -speed,
+            latest_y_movement: orientation,
+            y_movement: Enum.uniq([orientation | player.y_movement])
+        }
+
+      :down ->
+        %{
+          player
+          | vy: speed,
+            latest_y_movement: orientation,
+            y_movement: Enum.uniq([orientation | player.y_movement])
+        }
+
+      :left ->
+        %{
+          player
+          | vx: -speed,
+            latest_x_movement: orientation,
+            x_movement: Enum.uniq([orientation | player.x_movement])
+        }
+
+      :right ->
+        %{
+          player
+          | vx: speed,
+            latest_x_movement: orientation,
+            x_movement: Enum.uniq([orientation | player.x_movement])
+        }
     end
   end
 
   def stop_moving(%__MODULE__{} = player, orientation) do
     case orientation do
-      :up -> %{player | vy: 0}
-      :down -> %{player | vy: 0}
-      :left -> %{player | vx: 0}
-      :right -> %{player | vx: 0}
+      :up ->
+        if orientation == player.latest_y_movement do
+          y_movement = List.delete(player.y_movement, orientation)
+
+          if length(y_movement) == 1 do
+            %{player | vy: player.speed, y_movement: y_movement, latest_y_movement: :down}
+          else
+            %{player | vy: 0, y_movement: y_movement, latest_y_movement: nil}
+          end
+        else
+          y_movement = List.delete(player.y_movement, orientation)
+          %{player | y_movement: y_movement, latest_y_movement: :down}
+        end
+
+      :down ->
+        if orientation == player.latest_y_movement do
+          y_movement = List.delete(player.y_movement, orientation)
+
+          if length(y_movement) == 1 do
+            %{player | vy: player.speed * -1, y_movement: y_movement, latest_y_movement: :up}
+          else
+            %{player | vy: 0, y_movement: y_movement, latest_y_movement: nil}
+          end
+        else
+          y_movement = List.delete(player.y_movement, orientation)
+          %{player | y_movement: y_movement, latest_y_movement: :up}
+        end
+
+      :left ->
+        if orientation == player.latest_x_movement do
+          x_movement = List.delete(player.x_movement, orientation)
+
+          if length(x_movement) == 1 do
+            %{player | vx: player.speed, x_movement: x_movement, latest_x_movement: :right}
+          else
+            %{player | vx: 0, x_movement: x_movement, latest_x_movement: nil}
+          end
+        else
+          x_movement = List.delete(player.x_movement, orientation)
+          %{player | x_movement: x_movement, latest_x_movement: :right}
+        end
+
+      :right ->
+        if orientation == player.latest_x_movement do
+          x_movement = List.delete(player.x_movement, orientation)
+
+          if length(x_movement) == 1 do
+            %{player | vx: player.speed * -1, x_movement: x_movement, latest_x_movement: :left}
+          else
+            %{player | vx: 0, x_movement: x_movement, latest_x_movement: nil}
+          end
+        else
+          x_movement = List.delete(player.x_movement, orientation)
+          %{player | x_movement: x_movement, latest_x_movement: :doleftwn}
+        end
     end
   end
 
