a07d1e2b17842c14acea4104fe506f3dd04723a9
Craig Dazey <craigdazey@gmail.com>
2019-09-30 02:21:56 -0400

    add about info

--- lib/ETE_web/live/game_browser_live.ex
+++ lib/ETE_web/live/game_browser_live.ex
@@ -11,6 +11,7 @@ defmodule ETEWeb.GameBrowserLive do
     ETEWeb.GameBrowserView.render("game_browser.html", assigns)
   end
 
+  @impl true
   def mount(_session, socket) do
     worlds =
       Supervisor.list_children()
@@ -18,9 +19,15 @@ defmodule ETEWeb.GameBrowserLive do
 
     Process.send_after(self(), {:tick, @tick}, @tick)
 
-    {:ok, assign(socket, games: worlds)}
+    socket =
+      socket
+      |> assign(show_about: false)
+      |> assign(games: worlds)
+
+    {:ok, socket}
   end
 
+  @impl true
   def handle_info({:tick, time}, socket) do
     worlds =
       Supervisor.list_children()
@@ -31,14 +38,32 @@ defmodule ETEWeb.GameBrowserLive do
     {:noreply, assign(socket, games: worlds)}
   end
 
+  @impl true
   def handle_info(%{event: "add_game", payload: p}, socket) do
     {:noreply, assign(socket, games: [Server.get_world(p.id) | socket.assigns.games])}
   end
 
+  @impl true
   def handle_event("join_game", %{"id" => id}, socket) do
     {:noreply, live_redirect(socket, to: Routes.live_path(socket, ETEWeb.GameLive, id))}
   end
 
+  @impl true
+  def handle_event("show_about", _event, socket) do
+    {:noreply, assign(socket, show_about: true)}
+  end
+
+  @impl true
+  def handle_event("toggle_about", _event, socket) do
+    {:noreply, assign(socket, show_about: !socket.assigns.show_about)}
+  end
+
+  @impl true
+  def handle_event("hide_about", _event, socket) do
+    {:noreply, assign(socket, show_about: false)}
+  end
+
+  @impl true
   def handle_event("create_new_game", _value, socket) do
     id = Ecto.UUID.generate()
 
